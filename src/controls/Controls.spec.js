1783a3322c68455c982f13c10991b148606cb656
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-16 15:19:23 -0800

    buttons text changes to reflect the state the door will be in if clicked

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -3,12 +3,24 @@ import { render, rerender, fireEvent } from '@testing-library/react';
 import Controls from './Controls';
 
 test('provides buttons to toggle the closed and locked states', () => {
-  const { getByText, rerender, fireEvent } = render(<Controls locked={false} closed={false} />)
+  const { getByText, rerender } = render(<Controls locked={false} closed={false} />)
   getByText('Lock Gate');
   getByText(/close gate/i);
 
   rerender(<Controls locked={true} closed={true} />)
   getByText(/unlock gate/i);
   getByText(/open gate/i);
-
 })
+
+// test('buttons text changes to reflect the state the door will be in if clicked' , () => {
+//   const closedSpy = jest.fn();
+//   const lockedSpy = jest.fn();
+  
+//   const { getByText, rerender} = render(<Controls locked={false} closed={false} toggleClosed={closedSpy} toggleLocked={lockedSpy}/>)
+  
+//   const closeButton = getByText(/close gate/i);
+//   fireEvent.click(closeButton)
+//   getByText(/open gate/i);
+// })
+
+// test( 'the closed toggle button is disabled if the gate is locked', ())
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,6 @@
 // Test away
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, rerender, fireEvent } from '@testing-library/react';
 import Dashboard from './Dashboard';
 
 test('Dashboard defaults to Open and Unlocked', () => {
@@ -13,4 +13,24 @@ test('Dashboard shows Controls and Display', () => {
   const { getByTestId } = render(<Dashboard />)
   getByTestId('display');
   getByTestId('controls');
+})
+
+test('buttons text changes to reflect the state the door will be in if clicked' , () => {
+  const closedSpy = jest.fn();
+  const lockedSpy = jest.fn();
+  
+  const { getByText, rerender} = render(<Dashboard />)
+  
+  const closeButton = getByText(/close gate/i);
+  fireEvent.click(closeButton);
+  getByText(/open gate/i);
+  const lockButton = getByText(/lock gate/i);
+  fireEvent.click(lockButton);
+  getByText(/unlock gate/i);
+  const unlockButton = getByText(/unlock gate/i);
+  fireEvent.click(unlockButton);
+  getByText(/lock gate/i);
+  const openButton = getByText(/open gate/i);
+  fireEvent.click(openButton);
+  getByText(/close gate/i);
 })
