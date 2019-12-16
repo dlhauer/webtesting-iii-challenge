cb28e34485c2b552595d35fedb7109311c6ad1fd
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-16 14:02:03 -0800

    provides buttons to toggle the closed and locked states

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
-// Test away!
+import React from 'react';
+import { render, rerender, fireEvent } from '@testing-library/react';
+import Controls from './Controls';
+
+test('provides buttons to toggle the closed and locked states', () => {
+  const { getByText, rerender, fireEvent } = render(<Controls locked={false} closed={false} />)
+  getByText('Lock Gate');
+  getByText(/close gate/i);
+
+  rerender(<Controls locked={true} closed={true} />)
+  getByText(/unlock gate/i);
+  getByText(/open gate/i);
+
+})
