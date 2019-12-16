cefdf69886c49c93ffc921010237b4eb42a0ca84
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-16 12:55:46 -0800

    Gate defaults to Unlocked and Open

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test('Dashboard defaults to Open and Unlocked', () => {
+  const { getByText } = render(<Dashboard />)
+  getByText(/unlock/i);
+  getByText(/open/i);
+})
