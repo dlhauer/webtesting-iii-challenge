eec91c64097651b3988336e81cb576a6a743e5ce
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-16 13:16:55 -0800

    Display shows open/closed and locked/unlocked

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import { render, rerender } from '@testing-library/react';
+import Display from './Display';
+
+test('Display shows open/closed and locked/unlocked', () => {
+  const { getByText, rerender } = render(<Display />);
+  getByText(/unlocked/i);
+  getByText(/open/i);
+
+  rerender(<Display closed={true} locked={true}/>)
+  getByText(/locked/i);
+  getByText(/closed/i);
+})
