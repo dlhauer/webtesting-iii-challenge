ee9e2cd59e19c812d9280135ea6496b254e1fb99
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-16 13:03:04 -0800

    Dashboard shows controls and display

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -7,4 +7,10 @@ test('Dashboard defaults to Open and Unlocked', () => {
   const { getByText } = render(<Dashboard />)
   getByText(/unlock/i);
   getByText(/open/i);
+});
+
+test('Dashboard shows Controls and Display', () => {
+  const { getByTestId } = render(<Dashboard />)
+  getByTestId('display');
+  getByTestId('controls');
 })
